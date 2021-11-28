56a2b0708cdb8b69f73e0f516ca755267fa861d0
reham <reham.samy20122@gmail.com>
2021-11-28 14:53:59 +0200

    work

--- lib/cons.dart
+++ lib/cons.dart
@@ -8,6 +8,7 @@ class Cons {
   static var whiteFont=TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold );
   static var blackFont=TextStyle(color: Colors.black,fontSize: 15,fontStyle: FontStyle.italic);
   static var greyFont=TextStyle(color: Colors.grey,fontSize: 20,fontWeight: FontWeight.bold);
+  static var accentFont=TextStyle(color: Color.fromARGB(255,213,177,57),fontSize: 20,fontWeight: FontWeight.bold);
   static var blueFont=TextStyle(color: Color.fromARGB(255,123,196,229),fontSize: 20,fontWeight: FontWeight.bold);
   static var greyFont1=TextStyle(color: Colors.black54,fontSize: 18);
   static var greenFont= TextStyle(color: Colors.lightGreen);
--- lib/view/screens/product_item_detail_screen.dart
+++ lib/view/screens/product_item_detail_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:saaty_app/model/product_model.dart';
+
+import '../../cons.dart';
 
 class ProductItemDetailScreen extends StatefulWidget {
 
-  static String PRODUCT_DETAIL_ROUTE='/5';
+  static String PRODUCT_DETAIL_ROUTE='/6';
 
   @override
   _ProductItemDetailScreenState createState() => _ProductItemDetailScreenState();
@@ -17,26 +20,89 @@ class _ProductItemDetailScreenState extends State<ProductItemDetailScreen> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            expandedHeight: 300,
+            expandedHeight: 10,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               title: Text(product.name),
               centerTitle: true,
-              background: Hero(
-              tag: product.id,
-              child: FadeInImage(image: NetworkImage(product.images[0],scale: 1),
-                placeholder: AssetImage('watch_item1.png'),
-              fit: BoxFit.cover,),
-                ),
+              background:null
             ),
       
           ),
           SliverList(delegate: SliverChildListDelegate(
             [
-              Container(
-                height:700,
-                child: Text('ffffffffffffffff'),
-              )
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                     Container(
+                       width: 300,
+                       height: 300,
+                       child: Hero(
+                        tag: product.id,
+                        child: FadeInImage(image: NetworkImage(product.images[0],scale: 1),
+                          placeholder: AssetImage('watch_item1.png'),
+                          fit: BoxFit.cover,),
+                    ),
+                     ),
+                      Positioned(
+                        left: 10,
+                          bottom: 10,
+                          child: Icon(product.isFav==0?Icons.favorite:Icons.favorite_border,color: Colors.red,))
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(product.cat==0?'Watch':'Braclet',style: Cons.accentFont,),
+                      Text(product.price,style: Cons.greenFont,)
+                    ],
+                  ),SizedBox(height: 20,),
+                  Text(product.desc,),
+                  SizedBox(height: 20,),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    height: 80,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Cons.primary_color)
+                    ),
+                    child: Stack(
+                      children: [
+                        ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: product.images.length,
+                            itemBuilder: (_,indx){
+                              return Container(
+                                  margin:EdgeInsets.all(5)
+                                  ,child: Image.network(product.images[0],scale: 1,fit: BoxFit.cover,));
+                            }
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                           height: 80,
+                            width: 100,
+                            color: Cons.accent_color,
+                            child:Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(product.images.length.toString(),style: TextStyle(color: Colors.white),),
+                                Icon(Icons.photo_camera,color: Colors.white,),
+                                Icon(Icons.forward,color: Colors.white,)
+                              ],
+                            ) ,
+                          ),
+                        ),
+
+                      ],
+                    )
+
+                    )
+                ],
+              ),
             ]
           ))
         ],
