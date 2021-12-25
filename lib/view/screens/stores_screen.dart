cf9452f3db08e146cabfd5a3dbe579b759716646
Dell <reham.samy2012@yahoo.com>
2021-12-25 20:17:10 +0200

    yes

--- lib/view/screens/stores_screen.dart
+++ lib/view/screens/stores_screen.dart
@@ -27,57 +27,65 @@ class StoresScreen extends StatelessWidget {
               Expanded(flex:1,child: FlexibleSpaceBar(
                 title: Text('Name1'),
                 centerTitle: true,
+                background:  Column(
+                  children: [
+                    Expanded(flex:1,child:
+                    Card(
+                      margin: EdgeInsets.all(2),
+                      elevation: 6,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: TextFormField(
+                          controller: _searcController,
+                          focusNode: _textFocus,
+                          onChanged: onTextChange,
+                          decoration: InputDecoration(
+                              hintText: 'search',
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Cons.accent_color,
+                                size: 25,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  Icons.filter_list_alt,
+                                  color: Cons.accent_color,
+                                  size: 25,
+                                ),
+                                onPressed: () {
+                                  buildFilterDialogWidget(context);
+                                },
+                              ),
+                              // SizedBox(
+                              //     width:10,
+                              //     height:10,child: Image.asset('assets/images/nav_filter.png',width: 15,height: 15,)),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Cons.accent_color,
+                                  width: 1.0,
+                                ),
+                              )
+                            //ثى prefix: Icon(Icons.search,color: Cons.accent_color,)
+                          ),
+                        ),
+                      ),
+                    ),
+                    ),
+                    Expanded(
+                      flex:1,
+                      child: ListTile(
+                        title: Text('Name1'),
+                        leading: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('assets/images/store1.png'),
+                        ),
+                        trailing: IconButton(icon: Icon(Icons.message,color: Cons.accent_color,),),
+                      ),
+                    ),
+
+                  ],
+                ),
               )),
-               Expanded(flex:1,child:
-                   Card(
-                       margin: EdgeInsets.all(2),
-                       elevation: 6,
-                       child: Padding(
-                         padding: EdgeInsets.all(5),
-                         child: TextFormField(
-                           controller: _searcController,
-                           focusNode: _textFocus,
-                           onChanged: onTextChange,
-                           decoration: InputDecoration(
-                               hintText: 'search',
-                               prefixIcon: Icon(
-                                 Icons.search,
-                                 color: Cons.accent_color,
-                                 size: 25,
-                               ),
-                               suffixIcon: IconButton(
-                                 icon: Icon(
-                                   Icons.filter_list_alt,
-                                   color: Cons.accent_color,
-                                   size: 25,
-                                 ),
-                                 onPressed: () {
-                                   buildFilterDialogWidget(context);
-                                 },
-                               ),
-                               // SizedBox(
-                               //     width:10,
-                               //     height:10,child: Image.asset('assets/images/nav_filter.png',width: 15,height: 15,)),
-                               enabledBorder: UnderlineInputBorder(
-                                 borderSide: BorderSide(
-                                   color: Cons.accent_color,
-                                   width: 1.0,
-                                 ),
-                               )
-                             //ثى prefix: Icon(Icons.search,color: Cons.accent_color,)
-                           ),
-                         ),
-                       ),
-                     ),
-                   ),
-               Expanded(flex:2,child: ListTile(
-                 title: Text('Name1'),
-                 leading: CircleAvatar(
-                   radius: 50,
-                   backgroundImage: AssetImage('assets/images/store1.png'),
-                 ),
-                 trailing: IconButton(icon: Icon(Icons.message,color: Cons.accent_color,),),
-               ))
                  ],
               ),
            // ),
