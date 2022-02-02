a075a6729282daf56b1eda0b1e222f438b1b1e95
Dell <reham.samy2012@yahoo.com>
2022-02-02 20:15:33 +0200

    yes

--- lib/view/screens/product_item_detail_screen.dart
+++ lib/view/screens/product_item_detail_screen.dart
@@ -752,84 +752,84 @@ buildContainerProductImages(Product product){
 }
 
 
-
-  void showImageDialog(BuildContext context, Product product) {
-
-    showDialog(context: context, builder: (ctx){
-      return AlertDialog(
-        content: Container(
-          height: MediaQuery.of(ctx).size.height*0.7,
-          margin: EdgeInsets.symmetric(vertical: 0,horizontal: 0),
-          width: double.infinity,
-          child:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              buildCarouselSlider(product,context)  ,
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  for (int i =0;i<product.images.length;i++)
-                    buildCircleSlider(i)
-                ],
-              )
-
-            ],
-          ),
-
-        ),
-      );
-    });
-
-  }
-
-  Widget buildCircleSlider(int i){
-    return GetBuilder<ProductController>(
-      builder: (_)=>Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: Container(
-          height: 13,
-          width: 13,
-          decoration: BoxDecoration(
-              border:i==_productController.sliderIndex? Border.all(width:2,color:Cons.accent_color ):Border.all(width:1,color:Cons.accent_color ),
-              shape: BoxShape.circle,
-              color:_productController.sliderIndex==i?Colors.white:Cons.primary_color
-          ),
-        ),
-      ),
-    );
-  }
-
-
-  Widget buildCarouselSlider(Product product,BuildContext context) {
-    return
-      Container(
-        width: double.infinity,
-        height: MediaQuery.of(context).size.height *.4,
-        child:
-        GetBuilder<ProductController>(
-          builder: (_)=>
-           CarouselSlider(
-            carouselController:_carouselController,
-            items: product.images.map((e) =>
-                Image.network(e,fit: BoxFit.fill,)
-            ).toList(),
-            options: CarouselOptions(
-                onPageChanged: (ind,x){
-                 _productController.changeSliderImage(ind);
-                 _productController.update();
-                },
-                initialPage: 1,
-                autoPlayAnimationDuration:Duration(milliseconds: 100),
-                autoPlay: true,
-                enlargeCenterPage: true,
-                aspectRatio: 9/6,
-                viewportFraction: 0.97
-            ),),
-        ),
-
-      );
-  }
+  //
+  // void showImageDialog(BuildContext context, Product product) {
+  //
+  //   showDialog(context: context, builder: (ctx){
+  //     return AlertDialog(
+  //       content: Container(
+  //         height: MediaQuery.of(ctx).size.height*0.7,
+  //         margin: EdgeInsets.symmetric(vertical: 0,horizontal: 0),
+  //         width: double.infinity,
+  //         child:
+  //         Column(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             buildCarouselSlider(product,context)  ,
+  //             SizedBox(height: 20,),
+  //             Row(
+  //               mainAxisAlignment: MainAxisAlignment.center,
+  //               children: [
+  //                 for (int i =0;i<product.images.length;i++)
+  //                   buildCircleSlider(i)
+  //               ],
+  //             )
+  //
+  //           ],
+  //         ),
+  //
+  //       ),
+  //     );
+  //   });
+  //
+  // }
+  //
+  // Widget buildCircleSlider(int i){
+  //   return GetBuilder<ProductController>(
+  //     builder: (_)=>Padding(
+  //       padding: const EdgeInsets.all(2.0),
+  //       child: Container(
+  //         height: 13,
+  //         width: 13,
+  //         decoration: BoxDecoration(
+  //             border:i==_productController.sliderIndex? Border.all(width:2,color:Cons.accent_color ):Border.all(width:1,color:Cons.accent_color ),
+  //             shape: BoxShape.circle,
+  //             color:_productController.sliderIndex==i?Colors.white:Cons.primary_color
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+  //
+  //
+  // Widget buildCarouselSlider(Product product,BuildContext context) {
+  //   return
+  //     Container(
+  //       width: double.infinity,
+  //       height: MediaQuery.of(context).size.height *.4,
+  //       child:
+  //       GetBuilder<ProductController>(
+  //         builder: (_)=>
+  //          CarouselSlider(
+  //           carouselController:_carouselController,
+  //           items: product.images.map((e) =>
+  //               Image.network(e,fit: BoxFit.fill,)
+  //           ).toList(),
+  //           options: CarouselOptions(
+  //               onPageChanged: (ind,x){
+  //                _productController.changeSliderImage(ind);
+  //                _productController.update();
+  //               },
+  //               initialPage: 1,
+  //               autoPlayAnimationDuration:Duration(milliseconds: 100),
+  //               autoPlay: true,
+  //               enlargeCenterPage: true,
+  //               aspectRatio: 9/6,
+  //               viewportFraction: 0.97
+  //           ),),
+  //       ),
+  //
+  //     );
+  // }
 
 
